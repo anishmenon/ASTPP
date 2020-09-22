7892c80c0dd1e1084afdfcc1d1d39557ef886fcf
Anish Menon <anish@inzane.in>
2020-09-23 02:51:45 +0530

    Update astpp.lua

--- config/astpp.lua
+++ config/astpp.lua
@@ -1,7 +1,7 @@
 -- ASTPP Database Connection Information
 ODBC_DSN="ASTPP"
 DB_USERNAME="astppuser"
-DB_PASSWD="<PASSSWORD>"
+DB_PASSWD="nmto422vp4qsn67b"
 TBL_PREFIX=""
 -- Set to false if you want to bill for inbound calls --
 INB_FREE="TRUE"
